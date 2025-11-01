66f51da4da444ab2da292965d6386e61e43f3768
vergilcloud76-cmyk <vergilcloud76@gmail.com>
2025-11-01 20:36:24 +0200

    Update botController.js

--- server/controllers/botController.js
+++ server/controllers/botController.js
@@ -1,7 +1,11 @@
-const { Configuration, OpenAIApi } = require("openai");
+import { getAIResponse } from "../ai/openaiService.js";
 
-const configuration = new Configuration({
-  apiKey: OPENAI_API_KEY,
-});
-
-const openai = new OpenAIApi(configuration);
+export async function handleMessage(req, res) {
+  const { prompt } = req.body;
+  try {
+    const response = await getAIResponse(prompt);
+    res.json({ response });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+}
