64cb8864072ea30e1540fcf09de92227528c1076
vergilcloud76-cmyk <vergilcloud76@gmail.com>
2025-11-01 19:50:51 +0200

    Update env.js

--- config/env.js
+++ config/env.js
@@ -1,7 +1,5 @@
-import dotenv from 'dotenv';
-dotenv.config();
-
-export const MONGO_URI = process.env.MONGO_URI;
-export const OPENAI_KEY = process.env.OPENAI_KEY;
-export const WHATSAPP_TOKEN = process.env.WHATSAPP_TOKEN;
-export const TELEGRAM_TOKEN = process.env.TELEGRAM_TOKEN;
+module.exports = {
+  MONGO_URI: process.env.MONGO_URI,
+  OPENAI_API_KEY: process.env.OPENAI_API_KEY,
+  TELEGRAM_TOKEN: process.env.TELEGRAM_TOKEN
+};
