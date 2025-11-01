3cdfa9f62ddcbb4ba30a91e2de3262d510f0e5a3
vergilcloud76-cmyk <vergilcloud76@gmail.com>
2025-11-01 13:27:28 +0200

    Add RuleManager component for rule management

---
+++ dashboard/src/components/RuleManager.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function RuleManager() {
+  return (
+    <div>
+      <h2>إدارة القواعد</h2>
+      <p>هنا يمكن للتاجر إضافة أو تعديل قواعد الرد.</p>
+    </div>
+  );
+}
