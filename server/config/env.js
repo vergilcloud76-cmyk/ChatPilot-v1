7afe5d9fde523a6e065b61303241287fb48efac9
vergilcloud76-cmyk <vergilcloud76@gmail.com>
2025-11-01 20:25:42 +0200

    Create env.js

---
+++ server/config/env.js
@@ -0,0 +1,4 @@
+// server/config/env.js
+export const MONGO_URI = process.env.MONGO_URI;
+export const OPENAI_API_KEY = process.env.OPENAI_API_KEY;
+export const TELEGRAM_TOKEN = process.env.TELEGRAM_TOKEN;
