3ad40b4dff70d7dc8247a6d1917fbda06bb177f5
vergilcloud76-cmyk <vergilcloud76@gmail.com>
2025-11-02 00:10:45 +0200

    Update env.js

--- server/config/env.js
+++ server/config/env.js
@@ -1,4 +1,3 @@
-// server/config/env.js
 export const MONGO_URI = process.env.MONGO_URI;
 export const OPENAI_API_KEY = process.env.OPENAI_API_KEY;
 export const TELEGRAM_TOKEN = process.env.TELEGRAM_TOKEN;
