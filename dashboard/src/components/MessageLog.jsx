cd736f1ca67cb37d494869bffd37573a9c79381c
vergilcloud76-cmyk <vergilcloud76@gmail.com>
2025-11-01 13:28:34 +0200

    Add MessageLog component with Arabic text

---
+++ dashboard/src/components/MessageLog.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function MessageLog() {
+  return (
+    <div>
+      <h2>سجل الرسائل</h2>
+      <p>هنا يتم عرض كل الرسائل والردود.</p>
+    </div>
+  );
+}
