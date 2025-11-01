8af45e4441eb9b52ba706c4e2567d01a40225fdd
vergilcloud76-cmyk <vergilcloud76@gmail.com>
2025-11-01 20:35:20 +0200

    Update botController.js

--- server/controllers/botController.js
+++ server/controllers/botController.js
@@ -1,17 +1,7 @@
-const { Configuration, OpenAIApi } = require('openai');
-const { OPENAI_API_KEY } = require('../config/env.js');
+const { Configuration, OpenAIApi } = require("openai");
 
 const configuration = new Configuration({
   apiKey: OPENAI_API_KEY,
 });
+
 const openai = new OpenAIApi(configuration);
-
-async function generateResponse(prompt) {
-  const completion = await openai.createChatCompletion({
-    model: "gpt-3.5-turbo",
-    messages: [{ role: "user", content: prompt }],
-  });
-  return completion.data.choices[0].message.content;
-}
-
-module.exports = { generateResponse };
