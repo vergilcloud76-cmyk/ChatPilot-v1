cfde2b089306f2633bcb3427933f69d49673a62d
vergilcloud76-cmyk <vergilcloud76@gmail.com>
2025-11-02 03:19:29 +0200

    Update db.js

--- server/db.js
+++ server/db.js
@@ -1,17 +1,5 @@
-import mongoose from 'mongoose';
-import { MONGO_URI } from './config/env.js';
-
 const connectDB = async () => {
-  try {
-    await mongoose.connect(MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-    console.log('MongoDB connected ✅');
-  } catch (err) {
-    console.error('MongoDB connection error:', err);
-    process.exit(1);
-  }
+  ...
 };
 
 export default connectDB;
