84ff37cca33d400008a4c9ec5c7c0d858e5937d1
vergilcloud76-cmyk <vergilcloud76@gmail.com>
2025-11-02 02:55:47 +0200

    Update env.js

--- config/env.js
+++ config/env.js
@@ -1,5 +1,4 @@
-module.exports = {
-  MONGO_URI: process.env.MONGO_URI,
-  OPENAI_API_KEY: process.env.OPENAI_API_KEY,
-  TELEGRAM_TOKEN: process.env.TELEGRAM_TOKEN
-};
+import dotenv from 'dotenv';
+dotenv.config();
+
+export const MONGO_URI = process.env.MONGO_URI;
