e4fe7a3d34353c2a5d55a7792b6665633721637e
vergilcloud76-cmyk <vergilcloud76@gmail.com>
2025-11-03 05:56:47 +0200

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,32 +1,18 @@
 import express from "express";
-import cors from "cors";
-import morgan from "morgan";
 import helmet from "helmet";
+import morgan from "morgan";
 import dotenv from "dotenv";
-
 import connectDB from "./src/config/db.js";
-import mainRoutes from "./src/routes/main.js";
-import userRoutes from "./src/routes/users.js";
-import botRoutes from "./src/routes/bot.js";
+import mainRoute from "./src/routes/main.js";
 
 dotenv.config();
-const app = express();
-const PORT = process.env.PORT || 10000;
+connectDB();
 
-// Middlewares
-app.use(cors());
+const app = express();
 app.use(helmet());
 app.use(morgan("dev"));
 app.use(express.json());
+app.use("/", mainRoute);
 
-// Routes
-app.use("/", mainRoutes);
-app.use("/users", userRoutes);
-app.use("/bot", botRoutes);
-
-// Connect MongoDB
-connectDB();
-
-// Start server
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT} 🚀`));
-
