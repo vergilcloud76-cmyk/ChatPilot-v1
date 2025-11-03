cb73fe1f923209075f1237b60c913ac12d4b1952
vergilcloud76-cmyk <vergilcloud76@gmail.com>
2025-11-03 07:54:22 +0200

    Update App.js

--- client/public/src/App.js
+++ client/public/src/App.js
@@ -1,1 +1,25 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Dashboard from "./pages/Dashboard";
+import Messages from "./pages/Messages";
+import Settings from "./pages/Settings";
+import Logs from "./pages/Logs";
+import AIPlayground from "./pages/AIPlayground";
+import Navbar from "./components/Navbar";
+
+function App() {
+  return (
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Dashboard />} />
+        <Route path="/messages" element={<Messages />} />
+        <Route path="/settings" element={<Settings />} />
+        <Route path="/logs" element={<Logs />} />
+        <Route path="/ai" element={<AIPlayground />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
 
