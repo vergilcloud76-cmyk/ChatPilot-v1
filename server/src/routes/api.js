ba53db88027f6119bc2cf1a033a61f555f541a63
vergilcloud76-cmyk <vergilcloud76@gmail.com>
2025-11-03 07:53:15 +0200

    Update api.js

--- server/src/routes/api.js
+++ server/src/routes/api.js
@@ -1,1 +1,12 @@
+import express from "express";
+import Message from "../models/Message.js";
+const router = express.Router();
+
+// Get all messages
+router.get("/messages", async (req, res) => {
+  const messages = await Message.find().sort({ createdAt: -1 });
+  res.json(messages);
+});
+
+export default router;
 
