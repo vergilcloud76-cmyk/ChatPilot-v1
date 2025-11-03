bd2fd4c9d138b0d5ec1809fce2ade549ae459691
vergilcloud76-cmyk <vergilcloud76@gmail.com>
2025-11-03 18:10:26 +0200

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,19 +1,24 @@
 import express from "express";
 import cors from "cors";
 import bodyParser from "body-parser";
-import "./src/config/env.js";
+import dotenv from "dotenv";
 import { connectDB } from "./src/config/db.js";
 import apiRoutes from "./src/routes/api.js";
 import initBot from "./src/controllers/botController.js";
+
+dotenv.config();
 
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
+// Database
 connectDB();
-initBots(app);
+
+// Init Telegram Bot
+initBot();
 
 app.use("/api", apiRoutes);
 
 const PORT = process.env.PORT || 10000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT} 🚀`));
+app.listen(PORT, () => console.log(`✅ Server running on port ${PORT} 🚀`));
