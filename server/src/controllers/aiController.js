8ec57f2e3bac297a91efc4d12933b97f7e591fd2
vergilcloud76-cmyk <vergilcloud76@gmail.com>
2025-11-03 07:52:15 +0200

    Update aiController.js

--- server/src/controllers/aiController.js
+++ server/src/controllers/aiController.js
@@ -1,13 +1,12 @@
-const { Configuration, OpenAIApi } = require('openai');
+import { Configuration, OpenAIApi } from "openai";
+
 const configuration = new Configuration({ apiKey: process.env.OPENAI_API_KEY });
 const openai = new OpenAIApi(configuration);
 
-async function generateAIResponse(userMessage) {
+export const generateAIResponse = async (userMessage) => {
   const response = await openai.createChatCompletion({
     model: "gpt-4",
     messages: [{ role: "user", content: userMessage }]
   });
   return response.data.choices[0].message.content;
-}
-module.exports = { generateAIResponse };
-
+};
