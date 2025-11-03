ea4fda1f7e140cfcc262865b07368d92876b6166
vergilcloud76-cmyk <vergilcloud76@gmail.com>
2025-11-03 07:56:21 +0200

    Update Navbar.js

--- client/public/src/components/Navbar.js
+++ client/public/src/components/Navbar.js
@@ -1,1 +1,13 @@
+import { Link } from "react-router-dom";
+export default function Navbar() {
+  return (
+    <nav className="bg-gray-800 text-white p-4 flex space-x-4">
+      <Link to="/">Dashboard</Link>
+      <Link to="/messages">Messages</Link>
+      <Link to="/settings">Settings</Link>
+      <Link to="/logs">Logs</Link>
+      <Link to="/ai">AI Playground</Link>
+    </nav>
+  );
+}
 
