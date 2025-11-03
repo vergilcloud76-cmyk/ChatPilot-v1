c6c71d61bfec235061ed3f8a4f360369b4c3ae41
vergilcloud76-cmyk <vergilcloud76@gmail.com>
2025-11-03 07:51:52 +0200

    Update Message.js

--- server/src/models/Message.js
+++ server/src/models/Message.js
@@ -1,1 +1,11 @@
+import mongoose from "mongoose";
+
+const messageSchema = new mongoose.Schema({
+  platform: String,
+  chatId: String,
+  text: String,
+  createdAt: { type: Date, default: Date.now }
+});
+
+export default mongoose.model("Message", messageSchema);
 
