7b8d0160f0df50c44f9b4c3dd7edf22cf32b71f2
vergilcloud76-cmyk <vergilcloud76@gmail.com>
2025-11-03 07:55:05 +0200

    Update Messages.js

--- client/public/src/pages/Messages.js
+++ client/public/src/pages/Messages.js
@@ -1,1 +1,20 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+export default function Messages() {
+  const [messages, setMessages] = useState([]);
+  useEffect(() => {
+    axios.get("/api/messages").then(res => setMessages(res.data));
+  }, []);
+  return (
+    <div className="p-6">
+      <h1 className="text-2xl font-bold mb-4">Messages</h1>
+      {messages.map(msg => (
+        <div key={msg._id} className="border p-2 my-2">
+          <p><b>{msg.platform}</b> ({msg.chatId}): {msg.text}</p>
+        </div>
+      ))}
+    </div>
+  );
+}
 
