8fac212e66729b4bfec6bdea3165bff98850e03b
vergilcloud76-cmyk <vergilcloud76@gmail.com>
2025-11-03 09:49:45 +0200

    Update botController.js

--- controllers/botController.js
+++ controllers/botController.js
@@ -1,15 +1,8 @@
-import TelegramBot from "node-telegram-bot-api";
-import { askAI } from "./aiController.js";
+export function initBots() {
+    const token = process.env.TELEGRAM_TOKEN;
+    const bot = new TelegramBot(token, { polling: true });
 
-const bot = new TelegramBot(process.env.TELEGRAM_BOT_TOKEN, { polling: true });
-
-bot.on("message", async (msg) => {
-  const chatId = msg.chat.id;
-  const text = msg.text;
-
-  const reply = await askAI(text);
-  bot.sendMessage(chatId, reply);
-});
-
-export default bot;
-
+    bot.on("message", (msg) => {
+        bot.sendMessage(msg.chat.id, "البوت شغال 🚀");
+    });
+}
