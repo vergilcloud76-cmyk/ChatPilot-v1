db3585c69b81a169b80781e97b004b4dd3590113
vergilcloud76-cmyk <vergilcloud76@gmail.com>
2025-11-03 07:55:24 +0200

    Update Settings.js

--- client/public/src/pages/Settings.js
+++ client/public/src/pages/Settings.js
@@ -1,1 +1,9 @@
+export default function Settings() {
+  return (
+    <div className="p-6">
+      <h1 className="text-2xl font-bold mb-4">Settings</h1>
+      <p>هنا يمكن تحديث توكنات البوت وربط التطبيقات الأخرى</p>
+    </div>
+  );
+}
 
